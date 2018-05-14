d757a6eb531230f421322b6b2486433bbfcb908b
root <root@station177.example.com>
2018-05-14 13:16:32 +0530

    if code

---
+++ if.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python2
+import time
+print  "Press 1 to check current time :    "
+print  "Press 2:    "
+
+ch=raw_input()
+
+if   ch ==  '1'  :
+
+	print  time.ctime()
+
+
+else :
+
+	print  "no chance !!"
